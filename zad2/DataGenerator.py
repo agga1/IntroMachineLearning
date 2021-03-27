08ad0dba866a41c8feafb78333605670de5d7a5e
agga1 <agud100@gmail.com>
2021-03-27 14:08:29 +0100

    generating dataset

---
+++ zad2/DataGenerator.py
@@ -0,0 +1,43 @@
+from PIL import Image
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+RED = (237,  28,  36)
+GREEN = ( 34, 177,  76)
+BLUE = ( 63,  72, 204)
+color_to_class = {RED: 0, GREEN: 1, BLUE: 2}
+
+def load_data_from_image(filename, size=None):
+    image = Image.open(filename)
+    image.convert('RGB')
+    data = np.asarray(image)
+    print(data.shape)
+    all_points = []
+    for x in range(data.shape[0]):
+        for y in range(data.shape[1]):
+            color = tuple(data[x,y])
+            if color in color_to_class.keys():
+                new_point = [add_noise((x,y)), color_to_class[color]]
+                all_points.append(new_point)
+    if size is not None:
+        all_points = random.sample(all_points, size)
+    return to_standardized_notation(all_points)
+
+def add_noise(point2d, noise_radius=1):
+    x_noise = random.random() * noise_radius
+    y_noise = random.random() * noise_radius
+    return [point2d[1] + x_noise, point2d[0] + y_noise]
+
+
+def to_standardized_notation(dataset):
+    X = np.array([elem[0] for elem in dataset])
+    Y = np.array([elem[1] for elem in dataset])
+    return X,Y
+
+dataset = load_data_from_image('set.png', 1000)
+# print(dataset)
+# X1, Y1 = dataset
+# plt.scatter(X1[:, 0], X1[:, 1], marker='o', c=Y1,
+#             edgecolor='k')
+# plt.show()
---
+++ zad2/visualization.py
@@ -0,0 +1,100 @@
+from DataGenerator import dataset
+
+print(__doc__)
+
+
+# Code source: Gaël Varoquaux
+#              Andreas Müller
+# Modified for documentation by Jaques Grobler
+# License: BSD 3 clause
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+
+h = .02  # step size in the mesh
+
+classifiers = [
+    KNeighborsClassifier(1),
+    KNeighborsClassifier(9),
+    KNeighborsClassifier(13),
+]
+names = ["Nearest Neighbors 1", "Nearest Neighbors 1","Nearest Neighbors 1"]
+
+
+datasets = [
+    dataset,
+            ]
+
+
+figure = plt.figure(figsize=(27, 9))
+i = 1
+# iterate over datasets
+for ds_cnt, ds in enumerate(datasets):
+    # preprocess dataset, split into training and test part
+    X, y = ds
+    X = StandardScaler().fit_transform(X)
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=.4, random_state=42)
+
+    x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+    y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    print("xxyy",xx, yy)
+
+    # just plot the dataset first
+    ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+    if ds_cnt == 0:
+        ax.set_title("Input data")
+    # Plot the training points
+    ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train,
+               edgecolors='k')
+    # Plot the testing points
+    ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, alpha=0.6,
+               edgecolors='k')
+    ax.set_xlim(xx.min(), xx.max())
+    ax.set_ylim(yy.min(), yy.max())
+    ax.set_xticks(())
+    ax.set_yticks(())
+    i += 1
+
+    # iterate over classifiers
+    for name, clf in zip(names, classifiers):
+        ax = plt.subplot(len(datasets), len(classifiers) + 1, i)
+        clf.fit(X_train, y_train)
+        score = clf.score(X_test, y_test)
+        print(score)
+        # Plot the decision boundary. For that, we will assign a color to each
+        # point in the mesh [x_min, x_max]x[y_min, y_max].
+        if hasattr(clf, "decision_function"):
+            Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+        else:
+            Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        ax.contourf(xx, yy, Z, alpha=.8)
+
+        # Plot the training points
+        ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train,
+                   edgecolors='k')
+        # Plot the testing points
+        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test,
+                   edgecolors='k', alpha=0.6)
+
+        ax.set_xlim(xx.min(), xx.max())
+        ax.set_ylim(yy.min(), yy.max())
+        ax.set_xticks(())
+        ax.set_yticks(())
+        if ds_cnt == 0:
+            ax.set_title(name)
+        ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                size=15, horizontalalignment='right')
+        i += 1
+
+plt.tight_layout()
+plt.show()
